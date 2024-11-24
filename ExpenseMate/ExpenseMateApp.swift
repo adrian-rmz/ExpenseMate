77c767b9aee836496ce267d47a016200f1011d50
Adrián Alejandro Ramírez Cruz <adrian.rmz.587@gmail.com>
2024-11-24 18:08:05 +0100

    Initial Commit

---
+++ ExpenseMate/ExpenseMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseMateApp.swift
+//  ExpenseMate
+//
+//  Created by Adrián Alejandro Ramírez Cruz on 24/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
